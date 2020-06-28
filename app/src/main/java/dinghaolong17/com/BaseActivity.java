0bb3ea4150f12a6d0b9fcac5af17ed096269a325
admin <admin@gmail.com>
2020-06-28 10:31:17 +0800

    初始化项目

---
+++ app/src/main/java/dinghaolong17/com/BaseActivity.java
@@ -0,0 +1,11 @@
+package dinghaolong17.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    void initView(){};
+
+    void initData(){};
+
+}
